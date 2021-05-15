c2433a95ba597f6397ba810585d7bd85d6dd1573
naldal <cec7378@gmail.com>
2021-05-15 16:53:03 +0900

    Initial Commit

---
+++ SkyDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkyDetector
+//
+//  Created by 송하민 on 2021/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
